e6dcdc8adb02d927dcf96f1e59f42688cfc67cb1
claytonmgravatt <59989445+claytonmgravatt@users.noreply.github.com>
2020-07-01 13:06:50 -0700

    more sophisticated test

--- flasktest.py
+++ flasktest.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,request,render_template
+import time
 app = Flask(__name__)
 
 numberlist=[]
@@ -17,10 +18,6 @@ def number(first):
 
 def numbers():
     return str(numberlist)
-
-@app.route('/sum')
-def add():
-    return str(sum(numberlist))
 
 
 @app.route('/search/<number>')
@@ -45,9 +42,21 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     size = request.form['size']
-    processed_size = str(int(size)-1)
+    processed_size = str(size)
     processed_text=text.upper()
+    time.sleep(0)
     return render_template('return_form.html',processed_size=processed_size,processed_text=processed_text)
+
+@app.route('/inputtest')
+def myform():
+    return render_template('my_form.html')
+
+@app.route('/inputtest',methods=['Post'])
+def input_post():
+    text = request.form['text']
+    size = request.form['size']
+    return render_template('return_input.html',text=text)
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=int("777"),debug=True,use_reloader=False)
